e308342f4f9c82119b869f40bf2cb9d80c3d4549
Andrea Gilardi <andrea.gilardi@polimi.it>
2024-12-07 10:29:38 +0100

    add day 6 - part 2

--- code/day06.R
+++ code/day06.R
@@ -14,4 +14,25 @@ go_to_exit(
   nrow()
 
 # Second part -------------------------------------------------------------
-# ... 
+counter_loops <- 0L
+for (i in seq_len(nrow(input))) {
+  for (j in seq_len(ncol(input))) {
+    # Test whether we are at the starting point or at a boulder
+    if ((i == starting_point[1] && j == starting_point[2]) || input[i, j] == "#") {
+      next
+    }
+    
+    # Temporarily add a boulder
+    input[i, j] <- "#"
+    
+    # Check for a loop
+    if (check_for_loop(input, starting_point)) {
+      counter_loops <- counter_loops + 1L
+      print(paste0("Detected loop at i = ", i, " and j = ", j))
+    }
+    
+    # Restore matrix
+    input[i, j] <- "."
+  }
+}
+counter_loops
--- code/day06.cpp
+++ code/day06.cpp
@@ -39,15 +39,6 @@ bool check_boulder(const CharacterMatrix input, const int x, const int y) {
   return input(x - 1, y - 1) == "#";
 }
 
-// Helper function to check out-of-bounds
-// [[Rcpp::export]]
-bool check_oob(const CharacterMatrix input, const int x, const int y) {
-  return (x - 1) < 0 || 
-    (x - 1) >= input.nrow() || 
-    (y - 1) < 0 || 
-    (y - 1) >= input.ncol();
-}
-
 // Main function for the first part
 // [[Rcpp::export]]
 Rcpp::List go_to_exit(
@@ -55,37 +46,104 @@ Rcpp::List go_to_exit(
     const IntegerVector starting_point
 ) {
   // Initialize directions and positions
-  std::vector<char> directions = {'U'};
-  std::vector<int> positions_x = {starting_point[0]};
-  std::vector<int> positions_y = {starting_point[1]};
+  char direction = 'U'; 
+  std::size_t max_n = input.nrow() * input.ncol(); 
+  std::vector<int> positions_x (2 * max_n); 
+  positions_x[0] = starting_point[0]; 
+  std::vector<int> positions_y (2 * max_n);
+  positions_y[0] = starting_point[1]; 
   int next_x {0}; 
   int next_y {0}; 
   int counter {0}; 
   
   while (true) {
-    if (directions[counter] == 'U' || directions[counter] == 'D') {
-      next_x = get_next_step(positions_x[counter], directions[counter]); 
+    if (direction == 'U' || direction == 'D') {
+      next_x = get_next_step(positions_x[counter], direction); 
       next_y = positions_y[counter]; 
     } else {
       next_x = positions_x[counter]; 
-      next_y = get_next_step(positions_y[counter], directions[counter]); 
+      next_y = get_next_step(positions_y[counter], direction); 
     }
-    if (check_oob(input, next_x, next_y)) break;
+    // Check out of bounds
+    if ((next_x - 1) < 0 || (next_x - 1) >= input.nrow() || (next_y - 1) < 0 || (next_y - 1) >= input.ncol()) break;
+    
     if (check_boulder(input, next_x, next_y)) {
-      directions[counter] = turn_right(directions[counter]); 
+      direction = turn_right(direction); 
       continue; 
     }
     counter++; 
-    directions.push_back(directions[counter - 1]); 
-    positions_x.push_back(next_x); 
-    positions_y.push_back(next_y); 
+    positions_x[counter] = next_x; 
+    positions_y[counter] = next_y; 
   }
+  
+  positions_x.resize(counter + 1); 
+  positions_y.resize(counter + 1);
   
   return Rcpp::List::create(
     Named("x") = positions_x, 
     Named("y") = positions_y
   ); 
 }
+
+// Main function for the second part
+// [[Rcpp::export]]
+bool check_for_loop(
+    const CharacterMatrix input, 
+    const IntegerVector starting_point, 
+    const bool debug = false
+) {
+  bool detected_loop = false; 
+  
+  // Initialize directions and positions
+  char direction = 'U'; 
+  // I think this is the maximum number of different positions (???)
+  std::size_t max_n = input.nrow() * input.ncol(); 
+  int position_x = starting_point[0]; 
+  int position_y = starting_point[1]; 
+  int next_x {0}; 
+  int next_y {0}; 
+  std::size_t counter {0};
+  
+  if (debug) {
+    Rcpp::Rcout << "Printing debugging information" << std::endl; 
+    Rcpp::Rcout << "Starting point is (" << position_x << ", " << position_y << ")" << std::endl; 
+  }
+  
+  while (true) {
+    if (direction == 'U' || direction == 'D') {
+      next_x = get_next_step(position_x, direction); 
+      next_y = position_y; 
+    } else {
+      next_x = position_x; 
+      next_y = get_next_step(position_y, direction); 
+    }
+    // Check out of bounds
+    if ((next_x - 1) < 0 || (next_x - 1) >= input.nrow() || (next_y - 1) < 0 || (next_y - 1) >= input.ncol()) break;
+    
+    if (check_boulder(input, next_x, next_y)) {
+      direction = turn_right(direction); 
+      continue; 
+    }
+    counter++; 
+    position_x = next_x; 
+    position_y = next_y; 
+    
+    if (debug) {
+      Rcout << "Counter = " << counter << "; next_x = " << next_x << "; next_y = " << next_y << std::endl; 
+    }
+    
+    if (counter > max_n) {
+      if (debug) {
+        Rcout << "Detected loop" << std::endl;
+      }
+      detected_loop = true; 
+      break; 
+    }
+  }
+  
+  return detected_loop; 
+}
+
 
 /*** R
 stopifnot(get_next_step(5L, "L") == 4L)
@@ -106,12 +164,6 @@ tmp_input <- matrix(c(".", "#", ".", "."), nrow = 2, ncol = 2)
 
 stopifnot(check_boulder(tmp_input, 2L, 1L))
 stopifnot(!check_boulder(tmp_input, 1L, 1L))
-
-stopifnot(check_oob(tmp_input, 0L, 0L))
-stopifnot(check_oob(tmp_input, 3L, 1L))
-stopifnot(check_oob(tmp_input, 3L, 3L))
-stopifnot(check_oob(tmp_input, 1L, 3L))
-stopifnot(!check_oob(tmp_input, 1L, 1L))
 
 rm(tmp_input)
 */
